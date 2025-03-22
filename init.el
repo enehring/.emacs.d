6b10e819581c5018183434aea22d19b44dc30e10
Evan Nehring <enehring@agilefleet.com>
2025-03-21 20:44:07 -0400

    only set font when using GUI and move stuff around

--- init.el
+++ init.el
@@ -3,6 +3,27 @@
 (setq custom-file "./custom.el")
 (load custom-file)
 
+(column-number-mode)
+(global-display-line-numbers-mode)
+(menu-bar--display-line-numbers-mode-relative)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(global-hl-line-mode)
+(setq inhibit-startup-screen 1)
+
+;; TODO maybe save these to .emacs.d/ instead?
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; stole this from reddit (u/wojtek-graj)
+(when (window-system)
+  (cond
+   ((x-list-fonts "Jetbrains Mono")
+    (set-frame-font "Jetbrains Mono-10" nil t))
+   )
+)
+
 (use-package magit
   :ensure t
   :defer t)
@@ -12,20 +33,3 @@
   :defer t)
 
 (load-theme 'ef-melissa-light :no-confirm)
-
-(column-number-mode)
-(global-display-line-numbers-mode)
-(menu-bar--display-line-numbers-mode-relative)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(global-hl-line-mode)
-
-; TODO maybe save these to .emacs.d/ instead?
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-(setq inhibit-startup-screen 1)
-
-(add-to-list 'default-frame-alist
-	     '(font . "Jetbrains Mono-10"))

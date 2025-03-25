b3704773cbdc4df821609d6b5962b93c323dde2a
Evan Nehring <enehring@agilefleet.com>
2025-03-25 16:47:49 -0400

    I changed my mind. Move emacs-generated code back to init.el.

--- init.el
+++ init.el
@@ -1,21 +1,9 @@
 ;; for personal use only
 
-(setq use-package-always-ensure t)
-(setq use-package-always-defer t)
-
-(use-package modus-themes)
-
-(use-package magit)
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
-
-(customize-set-variable 'custom-enabled-themes '(modus-operandi-tinted))
 (customize-set-variable 'menu-bar-mode nil)
 (customize-set-variable 'tool-bar-mode nil)
 (customize-set-variable 'scroll-bar-mode nil)
 (customize-set-variable 'column-number-mode t)
-(customize-set-variable 'global-display-line-number-mode t)
 (customize-set-variable 'display-line-numbers 'relative)
 (customize-set-variable 'global-hl-line-mode t)
 (customize-set-variable 'inhibit-splash-screen t)
@@ -24,5 +12,22 @@
 (customize-set-variable 'make-backup-files nil)
 (customize-set-variable 'auto-save-default nil)
 
+
 (add-to-list 'default-frame-alist
 	     '(font . "Jetbrains Mono-10"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-operandi-tinted))
+ '(custom-safe-themes
+   '("712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db" default))
+ '(package-selected-packages '(modus-themes magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

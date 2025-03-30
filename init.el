bf30f575d84f44871638585eaf2612599be0e916
Evan Nehring <evan.d.nehring@gmail.com>
2025-03-30 16:28:48 -0400

    Install packages automatically and prevent ugly hashes.
    The idea is to get a fully configured emacs when loading for the first
    time with minimal user involvement. The confirmation to install
    selected packages remains just as a sanity check. The call to
    load-theme specifies the :no-confirm argument since it's reasonable to
    expect that if the package is selected, it's ok to run it's lisp code.

--- init.el
+++ init.el
@@ -21,9 +21,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-operandi-tinted))
- '(custom-safe-themes
-   '("712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db" default))
  '(package-selected-packages '(modus-themes magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -31,3 +28,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(unless (file-exists-p package-user-dir)
+  (package-refresh-contents)
+  (package-install-selected-packages))
+
+(load-theme 'modus-operandi-tinted :no-confirm)
+

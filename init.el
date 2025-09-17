0e6144a1d678128f7a61f63bfc44044d5797b14d
Evan Nehring <evan.d.nehring@gmail.com>
2025-09-17 14:09:28 -0400

    Remove modus-themes dependency.

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(go-mode magit modus-themes web-mode)))
+ '(package-selected-packages '(go-mode magit web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -42,11 +42,6 @@
   (package-install-selected-packages))
 
 ;;; Package configuration
-
-;;; modus-themes
-(customize-set-variable
- 'modus-themes-to-toggle
- '(modus-operandi-tinted modus-vivendi-tinted))
 
 ;;; web-mode
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
